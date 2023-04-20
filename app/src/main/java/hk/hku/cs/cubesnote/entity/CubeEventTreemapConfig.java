5b46d3d52b55e607d6bcb4d6c637cb561aef6071
1346798520 <yuanzhehui0325@outlook.com>
2023-04-21 03:21:26 +0800

    Fixed some bugs. Completed some TODOs.
    1. Completed TODO: deliver default calendar to treemap settings
    2. Optimized sync Button with selected Date & Time
    3. Fixed problem of month 20
    4. Completed passing treemap config obj between activities

--- app/src/main/java/hk/hku/cs/cubesnote/entity/CubeEventTreemapConfig.java
+++ app/src/main/java/hk/hku/cs/cubesnote/entity/CubeEventTreemapConfig.java
@@ -19,7 +19,51 @@ public class CubeEventTreemapConfig  implements Parcelable {
     private java.util.Calendar end;
     private Boolean linearEmergency;
     private java.util.Calendar linearEmergencyBegin;
-    // private final CubeEvent parent;
+
+    public CubeEventTreemapConfig(Calendar start, Calendar end, Calendar linear) {
+        this.importance = 3;
+        this.emergency = 3;
+        this.start = start;
+        this.end = end;
+        this.linearEmergency = false;
+        this.linearEmergencyBegin = linear;
+    }
+
+    protected CubeEventTreemapConfig(Parcel in) {
+        importance = in.readInt();
+        emergency = in.readInt();
+        start = Jsonfy.stringToCalendar(in.readString());
+        end = Jsonfy.stringToCalendar(in.readString());
+        linearEmergency = in.readByte() != 0;
+        linearEmergencyBegin = linearEmergency ? null : Jsonfy.stringToCalendar(in.readString());
+    }
+
+    public static final Creator<CubeEventTreemapConfig> CREATOR = new Creator<CubeEventTreemapConfig>() {
+        @Override
+        public CubeEventTreemapConfig createFromParcel(Parcel in) {
+            return new CubeEventTreemapConfig(in);
+        }
+
+        @Override
+        public CubeEventTreemapConfig[] newArray(int size) {
+            return new CubeEventTreemapConfig[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        parcel.writeInt(importance);
+        parcel.writeInt(emergency);
+        parcel.writeString(Jsonfy.calenderToString(start));
+        parcel.writeString(Jsonfy.calenderToString(end));
+        parcel.writeByte((byte) (linearEmergency ? 1 : 0));
+        parcel.writeString(Jsonfy.calenderToString(linearEmergencyBegin));
+    }
 
     public JSONObject toJson() throws JSONException {
         return Jsonfy.toJson(this);
