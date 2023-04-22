53f410a08cae29875ea84bb55c452e0c07e83572
1346798520 <yuanzhehui0325@outlook.com>
2023-04-22 19:36:02 +0800

    Added Calendar layout & activity

--- app/src/main/java/hk/hku/cs/cubesnote/ui/Calendar.java
+++ app/src/main/java/hk/hku/cs/cubesnote/ui/Calendar.java
@@ -1,6 +1,8 @@
 package hk.hku.cs.cubesnote.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -13,10 +15,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 import hk.hku.cs.cubesnote.R;
 
-public class Calendar extends AppCompatActivity {
+public class Calendar extends AppCompatActivity implements CalendarAdapter.OnItemListener {
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
     private ImageButton popBtn;
     private ImageButton setBtn;
     private Context myContext;
@@ -24,6 +36,9 @@ public class Calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_calendar);
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
         myContext = Calendar.this;
         popBtn = (ImageButton) findViewById(R.id.popBtn);
         setBtn = (ImageButton) findViewById(R.id.setBtn);
@@ -55,6 +70,65 @@ public class Calendar extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private void initWidgets()
+    {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+    private ArrayList<String> daysInMonthArray(LocalDate date)
+    {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+        int daysInMonth = yearMonth.lengthOfMonth();
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+        for(int i = 1; i <= 42; i++)
+        {
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
+            {
+                daysInMonthArray.add("");
+            }
+            else
+            {
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return  daysInMonthArray;
+    }
+    private String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+    public void previousMonthAction(View view)
+    {
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+    public void nextMonthAction(View view)
+    {
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+    @Override
+    public void onItemClick(int position, String dayText)
+    {
+        // add event here
+        if(!dayText.equals("")) {
+            String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
     }
     private void initPopWindow(View v) {
         View view = LayoutInflater.from(myContext).inflate(R.layout.popup_window, null, false);
